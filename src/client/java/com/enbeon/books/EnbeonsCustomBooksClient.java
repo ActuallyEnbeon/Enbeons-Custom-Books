f8ac56fa94a242b7f02a98c38c5b69bc7aa11c2c
Enbeon <85409980+ActuallyEnbeon@users.noreply.github.com>
2025-01-12 15:32:52 +0000

    Initial commit

---
+++ src/client/java/com/enbeon/books/EnbeonsCustomBooksClient.java
@@ -0,0 +1,10 @@
+package com.enbeon.books;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EnbeonsCustomBooksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
