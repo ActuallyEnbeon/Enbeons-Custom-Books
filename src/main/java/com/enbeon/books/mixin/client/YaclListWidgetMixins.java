929947757379d2b19b3469b1aa1175c49306ae22
Enbeon <85409980+ActuallyEnbeon@users.noreply.github.com>
2026-03-04 03:00:58 +0000

    Ensure add and remove buttons are only removed in this mod's screen

--- src/main/java/com/enbeon/books/mixin/client/YaclListWidgetMixins.java
+++ src/main/java/com/enbeon/books/mixin/client/YaclListWidgetMixins.java
@@ -1,42 +1,63 @@
 package com.enbeon.books.mixin.client;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.enbeon.books.config.controllers.DummyButtonWidget;
+import dev.isxander.yacl3.api.ListOption;
+import dev.isxander.yacl3.api.ListOptionEntry;
+import dev.isxander.yacl3.gui.AbstractWidget;
 import dev.isxander.yacl3.gui.OptionListWidget;
 import dev.isxander.yacl3.gui.TooltipButtonWidget;
+import dev.isxander.yacl3.gui.YACLScreen;
 import dev.isxander.yacl3.gui.controllers.ListEntryWidget;
-import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.contents.TranslatableContents;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Mutable;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
+@SuppressWarnings("unused")
 public class YaclListWidgetMixins {
     @Mixin(OptionListWidget.ListGroupSeparatorEntry.class)
     public static class DontRenderAddButton {
-        @WrapOperation(
-                method = "renderContent",
-                at = @At(value = "INVOKE", target = "Ldev/isxander/yacl3/gui/TooltipButtonWidget;render(Lnet/minecraft/client/gui/GuiGraphics;IIF)V")
+        @Mutable
+        @Shadow
+        @Final
+        private TooltipButtonWidget addListButton;
+
+        @Inject(
+                method = "<init>",
+                at = @At(value = "RETURN")
         )
-        private void renderBlocker(TooltipButtonWidget instance, GuiGraphics graphics, int mouseX, int mouseY, float deltaTicks, Operation<Void> original) {
-            // Don't render any buttons that are the add button
-            // TODO: check we're in the right screen
-            if (!instance.getMessage().getString().equals("+")) {
-                original.call(instance, graphics, mouseX, mouseY, deltaTicks);
+        private void renderBlocker(OptionListWidget this$0, ListOption<?> group, Screen screen, CallbackInfo ci) {
+            // If the current object is the enchantment order, replace the addListButton with a dummy
+            if (group.name().getContents() instanceof TranslatableContents contents
+                    && contents.getKey().equals("config.enbeons_custom_books.option.precedence"))
+            {
+                this.addListButton = new DummyButtonWidget(screen);
             }
         }
     }
 
     @Mixin(ListEntryWidget.class)
     public static class DontRenderRemoveButtons {
-        @WrapOperation(
-                method = "render",
-                at = @At(value = "INVOKE", target = "Ldev/isxander/yacl3/gui/TooltipButtonWidget;render(Lnet/minecraft/client/gui/GuiGraphics;IIF)V")
+        @Mutable
+        @Shadow
+        @Final
+        private TooltipButtonWidget removeButton;
+
+        @Inject(
+                method = "<init>",
+                at = @At(value = "RETURN")
         )
-        private void renderBlocker(TooltipButtonWidget instance, GuiGraphics graphics, int mouseX, int mouseY, float deltaTicks, Operation<Void> original) {
-            // Don't render any buttons that are the remove button
-            // TODO: check we're in the right screen
-            if (!instance.getMessage().getString().equals("\u274c")) {
-                original.call(instance, graphics, mouseX, mouseY, deltaTicks);
+        private void renderBlocker(YACLScreen screen, ListOptionEntry<?> listOptionEntry, AbstractWidget entryWidget, CallbackInfo ci) {
+            // If the current object is the enchantment order, replace the removeButton with a dummy
+            if (listOptionEntry.parentGroup().name().getContents() instanceof TranslatableContents contents
+                    && contents.getKey().equals("config.enbeons_custom_books.option.precedence"))
+            {
+                this.removeButton = new DummyButtonWidget(screen);
             }
         }
     }
